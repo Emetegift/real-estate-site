9f73f4964ab0d820ae3eb8591825c22954682695
Emete Gift <emeteokeoghene@gmail.com>
2023-08-05 19:32:11 +0100

    Built the app components

---
+++ src/main-page/header.js
@@ -0,0 +1,19 @@
+import React from "react";
+import logo from "./Home.png";
+import "./header.css"; // Import your CSS file with custom styles
+
+const Header = () => (
+    <header className="row">
+        <div className="col-md-5">
+            <img src={logo} className="logo" alt="Real Estate" />
+        </div>
+        <div className="col-md-7 mt-5 subtitle" style={{ color: "orange" }}>
+           <i> Providing comfortable homes all over the world</i>
+        </div>
+        <div className="col-md-12 mt-0.2">
+            <span className="real-homes">RealHomes</span>
+        </div>
+    </header>
+);
+
+export default Header;
