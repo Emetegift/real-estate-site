3640f2266f521953881ea63fcf9a564f905c40ea
Emete Gift <emeteokeoghene@gmail.com>
2023-08-06 06:56:35 +0100

    Adjusted the header

--- src/main-page/header.js
+++ src/main-page/header.js
@@ -6,13 +6,16 @@ const Header = ({subtitle}) => (
     <header className="row">
         <div className="col-md-5">
             <img src={logo} className="logo" alt="Real Estate" />
+            <div className="col-md-12 mt">
+                <span className="real-homes">RealHomes</span>
         </div>
-        <div className="col-md-7 mt-5 subtitle" style={{ color: "orange" }}>
+        </div>
+        <div className="col-md-7 mt-3 subtitle" style={{ color: "orange" }}>
             <i>{subtitle}</i>
         </div>
-        <div className="col-md-12 mt-0.2">
+        {/* <div className="col-md-12 mt-0.2">
             <span className="real-homes">RealHomes</span>
-        </div>
+        </div> */}
     </header>
 );
 
