255e01dc08091f3e10201a621efe080df7019c5a
Emete Gift <emeteokeoghene@gmail.com>
2023-08-07 07:44:45 +0100

    Defined a useNavigate hook

--- src/main-page/house-filter.js
+++ src/main-page/house-filter.js
@@ -1,14 +1,27 @@
+//import useNavigate hook to help navigate
+import { useNavigate } from "react-router-dom"; //This is formally "import { useHistory } from "react-router-dom";"
+
+
+  
+
 const HouseFilter = ({allHouses}) => {
+    const navigate = useNavigate(); //Call the hook
     const countries = allHouses
     ? Array.from(new Set(allHouses.map((h) => h.country)))
     : [];
     countries.unshift(null);
+    //Declare the function for the onSearchChange i.e the onChange handle
+    const onSearchChange = (e) => {
+        const country = e.target.value //Where e is same as onSearchChange
+        navigate.push(`/searchresults/${country}`);
+    }
     return ( 
         <div className="row mt-3">
             <div className="offset-md-2 col-md-4">
                 Look for your dream house in country:
             </div>
-            <div className="col-md-4 mb-3">
+            {/* the onChange handler will search for countries */}
+            <div className="col-md-4 mb-3" onChange={onSearchChange}> 
                 <select className="form-select">
                     {countries.map((c) => (
                         <option key ={c} value={c}>
